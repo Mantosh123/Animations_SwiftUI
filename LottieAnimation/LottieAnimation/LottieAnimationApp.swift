7f99939c5b0193c31c15cc6ed7d1741a5192e793
Mantosh Kumar <kumar.mantosh83@gmail.com>
2025-04-29 23:02:24 +0530

    Add new project for use lottie animations

---
+++ LottieAnimation/LottieAnimation/LottieAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationApp.swift
+//  LottieAnimation
+//
+//  Created by Mantosh Kumar on 29/04/25.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
